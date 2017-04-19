5afbbe0598e670635437275f2dca4230b14dadbf
Weiyao Ke <wk42@phy.duke.edu>
2017-04-19 16:05:23 -0400

    OSG version

--- run/plot_obs.py
+++ run/plot_obs.py
@@ -6,6 +6,8 @@ import sys
 import glob
 import fortranformat as ff
 from scipy.interpolate import interp1d
+import HepPlot
+from matplotlib.offsetbox import AnchoredText
 
 # pp->c reference
 ip = np.linspace(0.0,100,10000)
@@ -21,7 +23,7 @@ AA_Xc = dip*np.sum(ip*AA_dXc_dpT(ip))
 x2, y2 = np.loadtxt(sys.argv[3]).T
 pp_dXD_dpT = interp1d(x2, y2, fill_value='extrapolate')
 
-label = [r'$Duke, c$', r'$Duke, D$', r'$Marlene, c$', r'$Marlene, D$']
+label = [r'Duke $c$', r'Duke, $D$', r'Marlene, $c$', r'Marlene, $D$']
 
 def unpack_file(filename):
 	with h5py.File(filename, 'r') as f:
@@ -38,12 +40,12 @@ def unpack_file(filename):
 
 def get_Raa(pmu, weight, AAtot, ref):
 	pT = np.sqrt(pmu[0]**2 + pmu[1]**2)
-	H, be = np.histogram(pT, bins=200, range=[0.0, 50], normed=True, weights=weight)
+	H, be = np.histogram(pT, bins=100, range=[0.0, 50], normed=True, weights=weight)
 	x = (be[1:] + be[:-1])*0.5
 	return x, H/x/ref(x)*AAtot
 
 def get_v2(pmu, weight):
-	N = 17
+	N = 11
 	pTbins = np.exp(np.linspace(0., np.log(40.), N+1))-1.
 	pT = np.sqrt(pmu[0]**2 + pmu[1]**2)
 	q2 = (pmu[0]**2 - pmu[1]**2)/(pmu[0]**2 + pmu[1]**2)
@@ -58,45 +60,63 @@ def get_v2(pmu, weight):
 mx, myD, myc = np.loadtxt(sys.argv[4]).T
 for i, filename in enumerate(sys.argv[5:]):
 	Qp, QpT0, Qw, Dp, DpT0, Dw, ratio_DoverC = unpack_file(filename)
-	plt.subplot(3,1,1)
+	plt.subplot(2,1,1)
 	x, y = get_Raa(Qp, Qw, AA_Xc, pp_dXc_dpT)
 	plt.plot(x, y, 'b-', label = label[0])
 	plt.plot(mx, myc, 'r-', label = label[2])
 
-	plt.subplot(3,1,2)
+	plt.subplot(2,1,2)
 	print (ratio_DoverC)
 	x, y = get_Raa(Dp, Dw, AA_Xc*ratio_DoverC, pp_dXD_dpT)
 	plt.plot(x, y, 'b-', label = label[1])
 	plt.plot(mx, myD, 'r-', label = label[3])
-
+"""
 	plt.subplot(3,1,3)
 	x, y = get_v2(Qp, Qw)
 	plt.plot(x, y, 'y-' , label = label[0])
 	x, y = get_v2(Dp, Dw)
 	plt.plot(x, y, 'go--', label = label[1])
+"""
 
 
-plt.subplot(3,1,1)
-plt.plot([0,40],[1,1])
-plt.axis([0,40,0,2])
-plt.legend()
+plt.subplot(2,1,1)
+plt.axis([0,40,0,1.2])
 plt.xlabel(r'$p_T$ [GeV]')
 plt.ylabel(r'$R_{AA}$')
-	
-plt.subplot(3,1,2)
-plt.plot([0,40],[1,1])
-plt.axis([0,40,0,1.4])
-plt.legend()
+plt.xticks([])
+
+plt.legend(loc='best', framealpha=0.)
+
+ax = plt.subplot(2,1,2)
+plt.axis([0,40,0,1.1])
+
 plt.xlabel(r'$p_T$ [GeV]')
 plt.ylabel(r'$R_{AA}$')
+color = ['g','b','y']
+alpha = [0.2, 0.3, 0.4, 0.5]
 
+pi =  HepPlot.plot_info('/LHC-2760-PbPb-D-Raa-central/Table17.yaml')
+for i, p in enumerate(pi.plist):
+	ax.errorbar(p.x, p.y,[[-p.stat[0]], [p.stat[1]]], fmt='gD', markersize=5,
+				label='ALICE 0-20%' if i==0 else '')
+	for it, c, a in zip(p.syslist, color, alpha):
+		ax.fill_between([p.xl, p.xh], [p.y+it[0], p.y+it[0]],  [p.y+it[1], p.y+it[1]], alpha=a, color=c)
+#ax.set_xlabel(pi.xlabel)
+#ax.set_ylabel(pi.ylabel)
+#box = AnchoredText(pi.description, loc=1, frameon=False)
+#ax.add_artist(box)
+#pi.recommand_scale(ax)
+
+plt.legend(loc='best', framealpha=0.)
+"""
 plt.subplot(3,1,3)
 plt.plot([0,40],[0,0])
-plt.axis([0,40,-0.05, 0.15])
-plt.legend()
+plt.axis([0,40,-0.05, 0.25])
 plt.xlabel(r'$p_T$ [GeV]')
 plt.ylabel(r'$v_2$')
-	
-plt.suptitle("LHC 2760 GeV, 0-20%-averaged, $T_c = 168$ [MeV] \n Elastic $K=1$ FONLL-baseline")
+"""
+
+plt.suptitle("LHC 2760 GeV, Pb+Pb-->D+X\n"+r"Elastic $K=1$, self consistent $m_D$")
+plt.subplots_adjust(hspace=0.)
 	
 plt.show()
